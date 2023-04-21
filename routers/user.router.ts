25e57c147908dec762fb3eef17fa52389301a63d
kujawapatryk <homik363@wp.pl>
2023-04-21 19:37:21 +0200

    Adding check for matching credentials and ready for implementation with Passport

--- routers/user.router.ts
+++ routers/user.router.ts
@@ -1,7 +1,22 @@
 import {Request, Response, Router} from "express";
+import { UserRecord } from "../records/user.record";
+import { UserEntity } from "../types";
+import { checkPassword } from "../utils/checkPassword";
+import { ValidationError } from "../utils/errors";
 
 
 export const userRouter = Router()
   .post("/login/:mail/:password", async (req: Request, res: Response) => {
-    const {mail,password} = req.params;
+    const params= {...req.params,id: '',salt: '',} as UserEntity;
+    const newParams = new UserRecord(params);
+
+     if(await checkPassword(newParams)){
+         console.log('Dane logowania są prawidłowe');
+         //TODO: miejsce na ustawienie tokena passport
+     }else{
+         throw new ValidationError("Błędne hasło")
+     }
+
+
+
   });
