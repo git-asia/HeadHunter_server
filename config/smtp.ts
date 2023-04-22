c8c8b7515b4e4e3307f56a992255c9950bab6c50
kujawapatryk <homik363@wp.pl>
2023-04-22 06:30:37 +0200

    Creating a function for sending emails.

---
+++ config/smtp.ts
@@ -0,0 +1,12 @@
+export const smtpConfig =
+  {
+
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    secure: false, // true for 465, false for other ports
+    auth: {
+    user: "fb28fcbb25b468",
+      pass: "4c5cf2a5584eba"
+    }
+
+  }
--- utils/sendMail.ts
+++ utils/sendMail.ts
@@ -1,4 +1,26 @@
-export const sendMail = () =>{
+import nodemailer from "nodemailer";
+import { ValidationError } from "./errors";
+import { SentMessageInfo } from "nodemailer/lib/smtp-transport";
+import { smtpConfig } from "../config/smtp";
+
+export const sendMail =async (mail:string,subject:string, text:string, html:string):Promise<SentMessageInfo> =>{
+
+
+  const transporter = nodemailer.createTransport(smtpConfig);
+
+  const send = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>',
+    to: mail,
+    subject: subject,
+    text: text,
+    html: html,
+  });
+  console.log(send);
+  return send;
+
+  // process.on('uncaughtException', (err) => {
+  //   console.log('Nieobsłużony wyjątek: ', err);
+  // });
 
 
 }
