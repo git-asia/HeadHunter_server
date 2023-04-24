1caea9db28e0cfe606a99b35e0e47f06c933980e
kujawapatryk <homik363@wp.pl>
2023-04-25 00:53:51 +0200

    Add use handleError
    Add use handleError

--- index.ts
+++ index.ts
@@ -11,6 +11,7 @@ const app = express();
 // app.use(cors({
 //     origin: 'http://localhost:3000,' }));
 app.use(json());
+app.use(handleError);
 
 app.listen(3001,'0.0.0.0', () =>{
     console.log('Listening on http://localhost:3001');
