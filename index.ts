dfd95f45902e4aaf230fc715dfbfcc236eb62a84
kujawapatryk <homik363@wp.pl>
2023-04-25 01:37:42 +0200

    Removal of unnecessary code.
    Removal of unnecessary code.

--- index.ts
+++ index.ts
@@ -10,16 +10,6 @@ const app = express();
 //     origin: 'http://localhost:3000,' }));
 app.use(json());
 
-// app.use('/user', userRouter);
-// const data = {
-//     mail: 'homik363@o2.pl',
-//     password: 'homik1@H'
-// };
-//
-// const jsonData = JSON.stringify(data);
-// console.log(jsonData);
-
-
 app.listen(3001,'0.0.0.0', () =>{
     console.log('Listening on http://localhost:3001');
 });
