bf87b5cf807a5da185f196c67dd300577385349a
kujawapatryk <homik363@wp.pl>
2023-04-25 00:44:31 +0200

    Update index.ts
    Removal of unnecessary code.

--- index.ts
+++ index.ts
@@ -12,18 +12,6 @@ const app = express();
 //     origin: 'http://localhost:3000,' }));
 app.use(json());
 
-try{
-    const result =sendMail('homik363@wp.pl','tytuł jakiś','tresc wiadomości', '<b> jestem sobie html </b>');
-    console.log(result);
-}catch (err){
-    console.log(err);
-    throw new ValidationError('E-mail nie został wysłany');
-}
-
-console.log(sendMail('homik363@wp.pl','tytuł jakiś','tresc wiadomości', '<b> jestem sobie html </b>')) ;
-
-
-
 app.listen(3001,'0.0.0.0', () =>{
     console.log('Listening on http://localhost:3001');
 });
