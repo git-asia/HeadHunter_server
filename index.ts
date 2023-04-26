d2978c541f2f58b278eb9c54c057080defe415ab
JaskierBard <mateuszswiderski009@gmail.com>
2023-04-26 12:48:12 +0200

    changing routes location

--- index.ts
+++ index.ts
@@ -1,28 +1,21 @@
 import express, {json} from "express";
 // import cors from 'cors';
 import 'express-async-errors';
-import { adminRouter } from "./routers/admin.router";
-import { authRouter } from "./routers/auth.router";
-import { homeRouter } from "./routers/home.router";
-import { studentRouter } from "./routers/student.router";
-import { userRouter } from "./routers/user.router";
-
+import {adminRouter} from "./routers/admin.router";
+import {homeRouter} from "./routers/home.router";
+import {studentRouter} from "./routers/student.router";
+import {userRouter} from "./routers/user.router";
 
 const app = express();
 
-// app.use(cors({
-//     origin: 'http://localhost:3000,' }));
+// app.use(cors({     origin: 'http://localhost:3000,' }));
 app.use(json());
 
-app.use('/students', userRouter);
-app.use('/login', authRouter);
+app.use('/user', userRouter);
+app.use('/students', studentRouter);
 app.use('/manage', adminRouter);
-app.use('/about', studentRouter);
 app.use('/', homeRouter);
 
-
-
-
-app.listen(3001,'0.0.0.0', () =>{
+app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on http://localhost:3001');
 });
