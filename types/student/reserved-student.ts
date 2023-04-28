8ef779b9daa301e16b32015b2972b0a34bedc738
mic-dem <michal.demski@interia.pl>
2023-04-28 21:48:45 +0200

    Added interface reserved-student

---
+++ types/student/reserved-student.ts
@@ -0,0 +1,15 @@
+export interface ReservedStudent {
+    name: string;
+    avatar: string;
+    courseDegree: number;
+    activityDegree: number;
+    projectDegree: number;
+    teamProjectDegree: number;
+    expectedTypeWork: number;
+    expectedWorkCity: string;
+    expectedContractType: number;
+    expectedSalary: number;
+    canTakeApprenticeship: boolean;
+    monthsOfCommercialExp: number;
+    reservationTo: Date;
+}
