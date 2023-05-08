8a9fe7f82bece168209c00b1bf7c647b66dbb728
Marcel <zseg1ipg.mendrok.marcel@gmail.com>
2023-05-08 20:33:54 +0200

    fixed ESLINT problems

--- index.ts
+++ index.ts
@@ -1,21 +1,17 @@
-import express, {json} from "express";
+import express, { json } from 'express';
 // import cors from 'cors';
 import 'express-async-errors';
-import { userRouter } from "./routers/user.router";
 
-
-import {adminRouter} from "./routers/admin.router";
-import {homeRouter} from "./routers/home.router";
-import {studentRouter} from "./routers/student.router";
-import {userRouter} from "./routers/user.router";
-import {handleError} from "./utils/errors";
-
+import { adminRouter } from './routers/admin.router';
+import { homeRouter } from './routers/home.router';
+import { studentRouter } from './routers/student.router';
+import { userRouter } from './routers/user.router';
+import { handleError } from './utils/errors';
 
 const app = express();
 
 // app.use(cors({
 //     origin: 'http://localhost:3000,' }));
-
 
 app.use(json());
 
@@ -25,7 +21,6 @@ app.use('/user', userRouter);
 app.use('/students', studentRouter);
 app.use('/manage', adminRouter);
 app.use('/', homeRouter);
-
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on http://localhost:3001');
