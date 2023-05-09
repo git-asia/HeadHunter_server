c4b35c0dcedef7886df83bf8f2f1f4a2556d1073
kujawapatryk <homik363@wp.pl>
2023-05-09 02:41:07 +0200

    adding an HR user to the database and sending an email with registration information

--- routers/admin.router.ts
+++ routers/admin.router.ts
@@ -1,12 +1,39 @@
 import { Router } from "express";
+import { v4 as uuid } from "uuid";
+import { UserRecord } from "../records/user.record";
+import { HrRecord } from "../records/hr.record";
+import { sendMail } from "../utils/sendMail";
+
 
 export const adminRouter = Router();
-
 // zalogowany admin
 adminRouter
+  .get('/add-hr/:email/:fullName/:company/:maxReservedStudents',async (req,res) =>{
+    const {email,fullName,company,maxReservedStudents} = req.params
+
+    const userId = uuid();
+    const userData = {
+      userId,
+      email,
+    }
+    const hrData = {
+      hrId: userId,
+      fullName,
+      company,
+      maxReservedStudents: Number(maxReservedStudents),
+    }
+    const addUser = new UserRecord(userData);
+    const addHr = new HrRecord(hrData);
+
+    await addUser.insert();
+    await addHr.insert();
+    await sendMail(email,'MegaK Head hunter - rejestracja',"jakś wiadomośc z linkiem aktywacyjnym  http://adres.pl/aktywacja/TOKEN"); //TODO Dodanie generowanie tokenu,  dodanie tekstu maila
+
+  })
+
 
     .get('/list-students', async (req, res) => {
-        // wylistowanie wszystkich dostępnych kursantów 
+        // wylistowanie wszystkich dostępnych kursantów
     })
 
     .get('/list-hrs', async (req, res) => {
