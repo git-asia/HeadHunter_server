c396c6d41a6ee3de76f03ca2ffdae55c69c975f2
kujawapatryk <homik363@wp.pl>
2023-05-11 21:38:44 +0200

    Receiving data from the frontend

--- routers/student.router.ts
+++ routers/student.router.ts
@@ -5,21 +5,27 @@ export const studentRouter = Router();
 
 studentRouter
 
-    .get('/all', async (req, res) => {
-        const {...data } = req.query;        
-        const studentsShortinfo = await StudentRecord.getFilteredAll(data);
-        res.json(studentsShortinfo);
+    .get('/all/:remoteWork/:inOffice/:employmentContract/:mandateContract/:b2b/:workContract/:min/:max/:courseCompletion/:courseEngagement/:projectDegree/:teamProjectDegree/:canTakeApprenticeship/:monthsOfCommercialExp/:page/:rowsPerPage', async (req, res) => {
+        console.log(req.params);
+        console.log('tutaj jestem');
+
     })
 
     .patch('/status', async (req, res) => {
         const {action,studentId, hrId = null}:UpdateStatus = req.body;
         const message = await StudentRecord.statusChange(action,studentId,hrId);
-        res.status(200).json({ success: true, message: message });;
+        res.status(200).json({ success: true, message: message });
     })
 
+    .get('/short/:studentId', async (req, res) => {
+//     const studentsShortinfo = await StudentRecord.studentShortInfo(
+//         req.params.studentId
+//     );
+//     res.json(studentsShortinfo[0]);
+})
     
     .get('/getcv/:studentId', async (req, res) => {
-        const studentId = req.params.studentId
+       // const studentId = req.params.studentId
 
         // wymaga id studenta i zwraca wszystkie dane wymagane do wyświetlenia cv
         // kursanta (makieta 6)
