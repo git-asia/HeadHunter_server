95c9222ce734919c34df9e99007f0694393c57f2
kujawapatryk <homik363@wp.pl>
2023-05-18 20:12:11 +0200

    test Update db.ts

--- config/db.ts
+++ config/db.ts
@@ -2,9 +2,9 @@ import {createPool} from "mysql2/promise";
 import { config } from "./config";
 
 export const pool = createPool({
-    host: config.dbHost,
-    user: config.dbUser,
-    password: config.dbPassword,
-    database: config.dbDatabase,
+    host: 'containers-us-west-131.railway.app',
+    user: 'root',
+    password: 'po61hpY7GoW0IgXTv3gm',
+    database: super2,
     port: 5769,
 })
