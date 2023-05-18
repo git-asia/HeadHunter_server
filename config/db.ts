07e5fe2cc76ac07ccfc1ad9c5eb9750d0e270c0f
Joanna <tuszynska.joanna@gmail.com>
2023-05-18 21:05:54 +0200

    Reversed to variables.

--- config/db.ts
+++ config/db.ts
@@ -1,10 +1,10 @@
-import {createPool} from "mysql2/promise";
-import { config } from "./config";
+import { createPool } from 'mysql2/promise';
+import { config } from './config';
 
 export const pool = createPool({
-    host: 'containers-us-west-131.railway.app',
-    user: 'root',
-    password: 'po61hpY7GoW0IgXTv3gm',
-    database: 'super2',
-    port: 5769,
-})
+  host: config.dbHost,
+  user: config.dbUser,
+  password: config.dbPassword,
+  database: config.dbDatabase,
+  port: Number(config.dbPort),
+});
