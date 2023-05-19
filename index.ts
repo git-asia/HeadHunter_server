13cadcd4551f3353419c6416c5abbcee8fc101d7
Joanna <tuszynska.joanna@gmail.com>
2023-05-19 21:07:16 +0200

    Code refactor

--- index.ts
+++ index.ts
@@ -1,36 +1,28 @@
-import express, { json, Router } from "express";
+import express, { json } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
-import {adminRouter} from "./routers/admin.router";
-import {homeRouter} from "./routers/home.router";
-import {studentRouter} from "./routers/student.router";
-import {userRouter} from "./routers/user.router";
-import {handleError} from "./utils/errors";
-import { config } from "./config/config";
+import { adminRouter } from './routers/admin.router';
+import { homeRouter } from './routers/home.router';
+import { studentRouter } from './routers/student.router';
+import { userRouter } from './routers/user.router';
+import { handleError } from './utils/errors';
+import { config } from './config/config';
 
 const app = express();
 
-app.use(cors({
-    origin: config.corsOrigin }));
+app.use(
+  cors({
+    origin: config.corsOrigin,
+  }),
+);
 
 app.use(json());
 app.use(handleError);
-
 
 app.use('/user', userRouter);
 app.use('/student', studentRouter);
 app.use('/manage', adminRouter);
 app.use('/', homeRouter);
-
-//const router = Router();
-//app.use('/app', router);
-//router.use('/user', userRouter);
-//router.use('/student', studentRouter);
-//router.use('/manage', adminRouter);
-//router.use('/', homeRouter);
-
-
-
 
 app.listen(3001, 'localhost', () => {
   console.log('Listening on http://localhost:3001');
