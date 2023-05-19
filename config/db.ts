e866bcfafaf7e592413d1555091a325ca81df814
git-asia <94705253+git-asia@users.noreply.github.com>
2023-05-19 23:29:49 +0200

    Cleaning db.ts

--- config/db.ts
+++ config/db.ts
@@ -1,6 +1,5 @@
 import { config } from './config';
-import { createPool, PoolConnection } from 'mysql2/promise';
-import { RowDataPacket } from 'mysql2';
+import { createPool } from 'mysql2/promise';
 
 export const pool = createPool({
   host: config.dbHost,
@@ -8,40 +7,7 @@ export const pool = createPool({
   password: config.dbPassword,
   database: config.dbDatabase,
   port: config.dbPort,
-  socketPath: config.dbSocket,
   namedPlaceholders: true,
   decimalNumbers: true,
 });
 
-/// Test the connection
-(async () => {
-  let connection: PoolConnection | null = null;
-  try {
-    connection = await pool.getConnection();
-    console.log('Connected to MySQL database!');
-  } catch (err) {
-    console.error('Error connecting to MySQL:', err);
-  } finally {
-    if (connection) {
-      connection.release();
-    }
-  }
-})();
-
-// Use the connection pool for executing queries
-(async () => {
-  let connection: PoolConnection | null = null;
-  try {
-    connection = await pool.getConnection();
-    const [rows] = await connection.query<RowDataPacket[]>(
-      'SELECT `studentId` FROM `students`',
-    );
-    console.log('Query results:', rows);
-  } catch (err) {
-    console.error('Error executing query:', err);
-  } finally {
-    if (connection) {
-      connection.release();
-    }
-  }
-})();
