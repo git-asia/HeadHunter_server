9c16161b85478421b579830066637c31577cb1ac
git-asia <94705253+git-asia@users.noreply.github.com>
2023-05-20 03:10:54 +0200

    Update config.ts

--- config/config.ts
+++ config/config.ts
@@ -4,7 +4,7 @@ export const config = {
   dbPassword: process.env.MYSQLPASSWORD,
   dbDatabase: process.env.MYSQLDATABASE,
   dbPort: process.env.MYSQLPORT,
-  sqlUrl: process.env.MYSQL_URL,
+  sqlUrl: process.env.DATABASE_URL,
   corsOrigin: process.env.CORS_ORIGIN,
   appPort: process.env.PORT || 3001,
 };
